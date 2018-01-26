7d4da3204a636b4558610b5ec102a876b3816a5a
Busyreadingsomething <oro1919@gmail.com>
2018-01-26 15:45:11 -0800

    Finished send and fetch methods. Added underscore src to index.html. Installed underscore through Bower.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,39 @@
-// YOUR CODE HERE:
+//create class called App
+//create a constructor with no parameters
+//create an instance of the class, App and set it to a varable app
+class App {
+  constructor () {
+    this.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
+  }
+  init () {
+    
+  }
+
+  send (message) {
+    //invoke jquery ajax
+    $.ajax({
+      url: this.server,
+      type: 'POST',
+      data: message,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  }
+
+  //create a fetch method
+  fetch () {
+    // invoke the ajax
+    // GET
+    $.ajax({
+      url: this.server,
+      type: 'GET',
+    });
+  }
+}
+
+let app = new App();
